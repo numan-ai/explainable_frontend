9b538c385466962b56d7b4351e079c26e6858a79
Clementine2722 <84880070+Clementine2722@users.noreply.github.com>
2024-07-09 01:45:38 -0600

    Added Math Sources

--- src/sources.ts
+++ src/sources.ts
@@ -32,6 +32,30 @@ export type NumberSource = {
   type: "number";
 } & Source;
 
+export type AdditionSource = {
+  type: "addition";
+  first: Source;
+  second: Source;
+} & Source;
+
+export type SubtractionSource = {
+  type: "subtraction";
+  first: Source;
+  second: Source;
+} & Source;
+
+export type MultiplicationSource = {
+  type: "multiplication";
+  first: Source;
+  second: Source;
+} & Source;
+
+export type DivisionSource = {
+  type: "division";
+  first: Source;
+  second: Source;
+} & Source;
+
 
 export const getStructureFromSource = (base_structure: BaseStructure, source: Source): BaseStructure => {
   if (source === undefined || source.type === undefined) {
@@ -63,6 +87,38 @@ export const getStructureFromSource = (base_structure: BaseStructure, source: So
       return {
         "type": "number",
         "value": (source as NumberSource).value,
+      } as NumberStructure;
+    case "addition":
+      var add_number_source = source as AdditionSource;
+      var number1 = getStructureFromSource(base_structure, add_number_source.first) as NumberSource
+      var number2 = getStructureFromSource(base_structure, add_number_source.second) as NumberSource
+      return {
+        "type": "number",
+        "value": number1.value + number2.value,
+      } as NumberStructure;
+    case "subtraction":
+      var sub_number_source = source as SubtractionSource;
+      var number1 = getStructureFromSource(base_structure, sub_number_source.first) as NumberSource
+      var number2 = getStructureFromSource(base_structure, sub_number_source.second) as NumberSource
+      return {
+        "type": "number",
+        "value": number1.value - number2.value,
+      } as NumberStructure;
+    case "multiplication":
+      var mul_number_source = source as MultiplicationSource;
+      var number1 = getStructureFromSource(base_structure, mul_number_source.first) as NumberSource
+      var number2 = getStructureFromSource(base_structure, mul_number_source.second) as NumberSource
+      return {
+        "type": "number",
+        "value": number1.value * number2.value,
+      } as NumberStructure;
+    case "division":
+      var div_number_source = source as DivisionSource;
+      var number1 = getStructureFromSource(base_structure, div_number_source.first) as NumberSource
+      var number2 = getStructureFromSource(base_structure, div_number_source.second) as NumberSource
+      return {
+        "type": "number",
+        "value": number1.value / number2.value,
       } as NumberStructure;
     default:
       console.error("Unknown source type", source);
