edb27326cd208b3803c4a5683c734124836356e5
Kostya Cholak <kostya.cholak@gmail.com>
2024-07-13 23:01:10 +0400

    feat: add copy button

--- src/components/NoConnectionComponent.tsx
+++ src/components/NoConnectionComponent.tsx
@@ -1,4 +1,7 @@
 import Highlight from 'react-highlight'
+import { Button } from './ui/button';
+import { Check, ClipboardCopy } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 const exampleCode = `
 import time
@@ -22,12 +25,33 @@ while True:
 `.slice(1);
 
 export default function NoConnectionComponent() {
+  const [copied, setCopied] = useState(false);
+
   return (
-    <div>
-      <h1 className="text-xl font-extrabold mb-2">How to start</h1>
-      <Highlight className='python'>
-        { exampleCode }
-      </Highlight>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-xl font-extrabold mb-3">How to start</h1>
+      <div className="relative">
+        <Button 
+          variant="outline"
+          className="absolute right-[10px] top-[10px] w-10 h-10 p-0"
+          onClick={() => {
+            navigator.clipboard.writeText(exampleCode);
+            setCopied(true);
+            setTimeout(() => {
+              setCopied(false);
+            }, 1000);
+          }}
+        >
+          {!copied ? (
+            <ClipboardCopy className="h-4 w-4"/>
+          ) : (
+            <Check className="h-4 w-4"/>
+          )}
+        </Button>
+        <Highlight className="python">
+          { exampleCode }
+        </Highlight>
+      </div>
     </div>
   )
 }
